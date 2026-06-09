f8338ffcf5b5a488292e7a2ba0a4f807d39ec888
Jaiden <Jaiden.michiels@student.ehb.be>
2026-06-09 14:52:56 +0200

    wa ik nu heb werk verder arita

--- OrbitWebsite/main.js
+++ OrbitWebsite/main.js
@@ -2,10 +2,10 @@ let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 
-scrollContainer.addEventListener("wheel", (evt) => {
-	evt.preventDefault();
-	scrollContainer.scrollLeft += evt.deltaY;
-});
+// scrollContainer.addEventListener("wheel", (evt) => {
+// 	evt.preventDefault();
+// 	scrollContainer.scrollLeft += evt.deltaY;
+// });
 
 nextBtn.addEventListener("click", () => {
 	scrollContainer.style.scrollBehavior = "smooth";
