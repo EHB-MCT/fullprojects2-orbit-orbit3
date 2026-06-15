7733cac4239c23e507cd406beb6ae9a54972086f
Jaiden <Jaiden.michiels@student.ehb.be>
2026-06-15 11:02:14 +0200

    Negeer dit

--- OrbitWebsite/click.js
+++ OrbitWebsite/click.js
@@ -7,13 +7,24 @@ const descriptions = [
 	"Beschrijving van foto 6",
 ];
 
+const images = [
+	"images/TESTFOTO.png",
+	"images/VBfoto1.jpg",
+	"images/VBfoto3.jpg",
+	"images/VBfoto5.jpg",
+	"images/VBfoto4.png",
+	"images/VBfoto6.jpg",
+];
+
 let display = document.querySelector("#description-display");
+let infoImage = document.querySelector("#info-image");
 let spans = document.querySelectorAll(".gallery span");
 
 spans.forEach(function (span) {
 	span.addEventListener("mouseup", function () {
 		let index = span.dataset.index;
-		display.innerText = descriptions[index];
+		display.innerHTML = descriptions[index];
+		infoImage.src = images[index];
 
 		document.querySelector("#info-section").scrollIntoView({
 			behavior: "smooth",
