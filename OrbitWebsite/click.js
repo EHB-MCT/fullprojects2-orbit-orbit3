6fe689402ae531f83b04cb513a0896ab5962ef4a
Arita Koca <arita.koca@student.ehb.be>
2026-06-15 13:47:04 +0200

    bold txt 1

--- OrbitWebsite/click.js
+++ OrbitWebsite/click.js
@@ -1,11 +1,11 @@
 const descriptions = [
-	`Op 29/11 hebben we samen met Indus en hun organisaties een solidaire standenmarkt georganiseerd.
-	Er waren in totaal 70 bezoekers langsgekomen om de kraampjes te komen zien.
-	Onze motivatie is eenvoudig: een evenement creëren waar de gemeenschap samenkomt,
-	ongeacht afkomst en cultuur, om een belangrijk sociaal doel te steunen.
-	Lokale organisaties die actief zijn in de strijd tegen armoede werden ook uitgenodigd om deel te nemen en/of de markt te bezoeken met hun leden.
-	Er werden oplossingen voorzien om eventuele financiële obstakels weg te nemen, want we willen dat iedereen van dit moment kon genieten.
-	Het was een warm, solidair en toegankelijk evenement – voor een iets aangenamere maand november.`,
+	`Op <strong>29/11</strong> hebben we samen met <strong>Indus</strong> en hun organisaties een <strong>solidaire standenmarkt</strong> georganiseerd.
+	Er waren in totaal <strong>70 bezoekers</strong> langsgekomen om de kraampjes te komen zien.
+	Onze motivatie is eenvoudig: een <strong>evenement creëren waar de gemeenschap samenkomt</strong>,
+	ongeacht afkomst en cultuur, om een belangrijk sociaal <strong>doel te steunen</strong>.
+	Lokale organisaties die actief zijn in de <strong>strijd tegen armoede</strong> werden ook uitgenodigd om deel te nemen en/of de markt te bezoeken met hun leden.
+	Er werden <strong>oplossingen</strong> voorzien om eventuele <strong>financiële obstakels weg te nemen</strong>, want we willen dat iedereen van dit moment kon genieten.
+	Het was een <strong>warm, solidair en toegankelijk evenement</strong> voor een iets aangenamere maand november.`,
 	"Beschrijving van foto 2",
 	"Beschrijving van foto 3",
 	"Beschrijving van foto 4",
